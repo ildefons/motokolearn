62e9ac8ac2c5ecca15a8fad5446706e68fbad6d2
Ildefons Magrans de Abril <ildefons.magrans@gmail.com>
2023-10-16 10:45:06 +0200

    Initial commit.

---
+++ src/motokolearn_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { motokolearn_backend } from "../../declarations/motokolearn_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await motokolearn_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
